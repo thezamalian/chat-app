cddcc0eff6effdcf6a9ea9534136720537581a39
Zamal Hossain <thezamalian@gmail.com>
2022-02-23 00:32:25 +0600

    loginForm is developed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css'
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />
     return (
         <ChatEngine
             height="100vh"
             projectID="37d7bcd9-646e-4af5-8477-46e46bed029a"
-            userName="sabbir_ahmed"
-            userSecret="123456"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
